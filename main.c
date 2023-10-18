c7bc17048f9984d25cd6bef4fbd9c86283dbc985
rsth <nltabun@outlook.com>
2023-10-18 19:09:57 +0300

    user input, write_entries func, test

--- main.c
+++ main.c
@@ -9,10 +9,13 @@
 #define LINK_MAX 256
 #define LINE_MAX (ALIAS_MAX + LINK_MAX)
 #define FILE_NAME_MAX 96
+#define CMD_MAX 8
+#define INPUT_MAX LINK_MAX // Should be atleast equivalent to highest max
 #define DEFAULT_FILE_NAME "repositories.csv"
 #define HEADER "Alias,Link"
 #define ENTRY_FMT "%s,%s"
-#define SCAN_FMT "%[^,],%s"
+#define ENTRY_SCAN_FMT "%[^,],%s"
+#define CMD_FMT "%s %s %s"
 
 typedef struct RepositoryEntry
 {
@@ -23,22 +26,29 @@ typedef struct RepositoryEntry
 
 bool read_entry(FILE *file_ptr, const char *alias, const char *link);
 bool add_new_entry(RepositoryEntry **head, const char *alias, const char *link);
-bool write_entries(FILE *file_ptr, RepositoryEntry *entry);
+int write_entries(FILE *file_ptr, RepositoryEntry *entry);
 void show_link(RepositoryEntry *head, const char *alias);
 void print_all_aliases(RepositoryEntry *head);
 void delete_entry(RepositoryEntry *head, const char *alias);
 void free_list(RepositoryEntry *head);
 void print_commands();
+bool test(FILE *file_ptr, RepositoryEntry *head, const char *file_name, char *new_alias, char *new_link);
 
 int main(int argc, char const *argv[])
 {
     FILE *file_ptr;
     char file_name[FILE_NAME_MAX] = DEFAULT_FILE_NAME;
     char line[LINE_MAX];
+    bool quit = false;
     RepositoryEntry *head = NULL;
-    char new_alias[ALIAS_MAX] = "GitHub";
-    char new_link[LINK_MAX] = "https://github.com";
+    char user_input[INPUT_MAX];
+    char new_alias[ALIAS_MAX];
+    char new_link[LINK_MAX];
+    char command[CMD_MAX];
+    int command_args;
+    bool changes_saved;
 
+    // TODO? func ? init?
     // Check if file exists and if it doesn't, try to create it
     if (!(does_file_exist(file_ptr, file_name)))
     {
@@ -46,6 +56,7 @@ int main(int argc, char const *argv[])
         if (create_file(file_ptr, file_name, ENTRY_FMT, HEADER))
         {
             printf("Successfully created file %s\n", file_name);
+            changes_saved = true;
         }
         else
         {
@@ -54,85 +65,115 @@ int main(int argc, char const *argv[])
         }
     }
     else
+    {
         printf("File %s found.\n", file_name);
-        file_ptr = fopen(file_name, "r");
+        changes_saved = true;
+    }
 
-        // Check header TODO: Handle somehow
-        if (!(verify_line_from_file(file_ptr, HEADER, LINE_MAX)))
+    file_ptr = fopen(file_name, "r");
+
+    // Check header TODO: Handle somehow
+    if (!(verify_line_from_file(file_ptr, HEADER, LINE_MAX)))
+    {
+        printf("Missing correct header.\n");
+    }
+
+    // Read entries TODO: make more rigid, stops if bad line encountered (while feof, if read_entry?)
+    while (read_entry(file_ptr, new_alias, new_link))
+    {
+        if (!(add_new_entry(&head, new_alias, new_link)))
         {
-            printf("Missing correct header.\n");
+            printf("Failed to add entry\n");
         }
-
-        // Read entries TODO: make more rigid, stops if bad line encountered
-        while (read_entry(file_ptr, new_alias, new_link))
-        {
-            if (!(add_new_entry(&head, new_alias, new_link)))
-            {
-                printf("Failed to add entry\n");
-            }
-        }
-
-        fclose(file_ptr);
-
-    // Test print aliases
-    print_all_aliases(head);
-
-    // Test add
-    if (add_new_entry(&head, new_alias, new_link))
-    {
-        printf("Successfully added new entry: %s|%s\n", new_alias, new_link);
-    }
-    else
-    {
-        printf("Failed to add new entry: %s|%s\n", new_alias, new_link);
-    }
-
-    // Test print aliases
-    print_all_aliases(head);
-
-    // Test write to file
-    file_ptr = fopen(file_name, "w");
-    if (file_ptr == NULL)
-    {
-        printf("Failed to open file for writing.\n");
-        return EXIT_FAILURE;
-    }
-
-    // TODO: Prob should be function: write_entries(...)
-    RepositoryEntry *current = head;
-    if (write_to_file(file_ptr, HEADER) == 1)
-    {
-        printf("Successfully wrote header to file.\n");
-    }
-    else
-    {
-        printf("Failed to write to file.\n");
-    }
-    
-    while (current != NULL)
-    {
-        if (write_to_file(file_ptr, ENTRY_FMT, new_alias, new_link) == 1)
-        {
-            printf("Successfully wrote entry to file.\n");
-        }
-        else
-        {
-            printf("Failed to write entry to file.\n");
-        }
-        current = current->next;
     }
 
     fclose(file_ptr);
+    // ?TODO
 
-    // Test print aliases
-    print_all_aliases(head);
+    printf("Welcome to REPOSITORY MANAGER\nType \"help\" to list available commands\n");
+    // Main program loop
+    while (!quit)
+    {
+        printf(">");
+        if (fgets(user_input, sizeof(user_input), stdin) != NULL) // TODO: validation in function
+        {
+            user_input[strcspn(user_input, "\n")] = 0;
+            command_args = sscanf(user_input, CMD_FMT, command, new_alias, new_link);
+            printf("Args: %d\n", command_args);
+            printf("Cmd: %s, Alias: %s, Link: %s\n", command, new_alias, new_link);
+
+            if (strcmp(command, "quit") == 0)
+            {
+                if (!changes_saved)
+                {
+                    file_ptr = fopen(file_name, "w");
+                    if (file_ptr == NULL)
+                    {
+                        printf("Failed to open file for writing.\n");
+                        return EXIT_FAILURE;
+                    }
+
+                    if (write_to_file(file_ptr, HEADER) == 1)
+                    {
+                        printf("Successfully wrote header to file.\n");
+                    }
+                    else
+                    {
+                        printf("Failed to write header to file.\n");
+                    }
+
+                    int entry_w = write_entries(file_ptr, head);
+                    printf("Wrote %d entries to file.\n", entry_w);
+
+                    fclose(file_ptr);
+                }
+
+                printf("Quitting..\n");
+                quit = true;
+            }
+            else if (strcmp(command, "add") == 0)
+            {
+                if (!(add_new_entry(&head, new_alias, new_link)))
+                {
+                    printf("Failed to add entry\n");
+                }
+                else
+                {
+                    printf("Successfully added entry.\n");
+                    changes_saved = false;
+                }
+            }
+            else if (strcmp(command, "show") == 0)
+            {
+                /* code */
+            }
+            else if (strcmp(command, "list") == 0)
+            {
+                print_all_aliases(head);
+            }
+            else if (strcmp(command, "delete") == 0)
+            {
+                /* code */
+            }
+            else if (strcmp(command, "help") == 0)
+            {
+                /* code */
+            }
+            else
+            {
+                printf("Unknown command. Type \"help\" to list available commands\n");
+            }
+        }
+    }
+
+    // Run test
+    // test(file_ptr, head, file_name, new_alias, new_link);
 
     // Free memory
     free_list(head);
 
     return 0;
 }
-
 
 // Read entry from file and add it to list
 // TODO: add rigorous checking for invalid readings.
@@ -142,18 +183,32 @@ bool read_entry(FILE *file_ptr, const char *alias, const char *link)
 
     if (fgets(read_line, sizeof(read_line), file_ptr) == NULL)
         return false;
-    
-    if (sscanf(read_line, SCAN_FMT, alias, link) != 2)
-        return false;
 
-    printf("%s|%s|%s\n", read_line, alias, link);
+    if (sscanf(read_line, ENTRY_SCAN_FMT, alias, link) != 2)
+        return false;
 
     return true;
 }
 
-bool write_entries(FILE *file_ptr, RepositoryEntry *entry)
+int write_entries(FILE *file_ptr, RepositoryEntry *entry)
 {
-    return true;
+    RepositoryEntry *current = entry;
+    int count = 0;
+
+    while (current != NULL)
+    {
+        if (write_to_file(file_ptr, ENTRY_FMT, current->alias, current->link) == 1)
+        {
+            count++;
+        }
+        else
+        {
+            printf("Failed to write entry to file.\n");
+        }
+        current = current->next;
+    }
+
+    return count;
 }
 
 bool add_new_entry(RepositoryEntry **head, const char *alias, const char *link)
@@ -210,4 +265,51 @@ void free_list(RepositoryEntry *head)
         current = current->next;
         free(temp);
     }
+}
+
+bool test(FILE *file_ptr, RepositoryEntry *head, const char *file_name, char *new_alias, char *new_link)
+{
+    // Test print aliases
+    print_all_aliases(head);
+
+    // Test add
+    if (add_new_entry(&head, new_alias, new_link))
+    {
+        printf("Successfully added new entry: %s|%s\n", new_alias, new_link);
+    }
+    else
+    {
+        printf("Failed to add new entry: %s|%s\n", new_alias, new_link);
+    }
+
+    // Test print aliases
+    print_all_aliases(head);
+
+    // Test write to file
+    file_ptr = fopen(file_name, "w");
+    if (file_ptr == NULL)
+    {
+        printf("Failed to open file for writing.\n");
+        return false;
+    }
+
+    // Test write back to file
+    if (write_to_file(file_ptr, HEADER) == 1)
+    {
+        printf("Successfully wrote header to file.\n");
+    }
+    else
+    {
+        printf("Failed to write to file.\n");
+    }
+
+    int entry_w = write_entries(file_ptr, head);
+    printf("Wrote %d entries to file.\n", entry_w);
+
+    fclose(file_ptr);
+
+    // Test print aliases
+    print_all_aliases(head);
+
+    return true;
 }
